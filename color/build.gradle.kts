7deb201a5fb98fff2e2b270f490c36330b57d04b
paul-nguyen-goldenowl <104960766+paul-nguyen-goldenowl@users.noreply.github.com>
2024-05-27 23:45:27 +0700

    add color

---
+++ color/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+//
+//java {
+//    sourceCompatibility = JavaVersion.VERSION_1_8
+//    targetCompatibility = JavaVersion.VERSION_1_8
+//}
+//kotlin {
+//    jvmToolchain(8)
+//}
+android {
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 29
+    }
+    namespace = "com.ln.composables.color"
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        compose = true
+    }
+    buildTypes {
+        all {
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+            isMinifyEnabled = true
+        }
+    }
+}
+dependencies {
+    api(platform(libs.androidx.compose.bom))
+    api(libs.androidx.compose.ui)
+    api(libs.androidx.compose.runtime)
+    api(libs.androidx.core.ktx)
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.material3)
+
+}
